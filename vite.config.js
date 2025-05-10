95d1f71a060ccf28407795f86fea898c9e43c026
Maaz Ahmad Khan <maazahmadkhan@superfone.in>
2025-05-10 14:57:45 +0530

    icons changed

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         theme_color: "#317EFB",
         icons: [
           {
-            src: "/salah.png",
+            src: "/icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/salah.png",
+            src: "/icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
